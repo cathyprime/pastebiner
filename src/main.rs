ab100ed6802315352909a2c9415e62fec5a4f05f
Yoolayn <jaroszynskijulian@gmail.com>
2024-04-30 20:30:11 +0200

    refactor the code to not have so much indents

--- src/main.rs
+++ src/main.rs
@@ -135,131 +135,154 @@ fn cli() -> Command {
         )
 }
 
+fn handle_info(api_user_dev_key: String, api_user_key: String) -> Result<String, PastebinError> {
+    let v = api(multipart::Form::new()
+        .text("api_dev_key", api_user_dev_key)
+        .text("api_user_key", api_user_key)
+        .text("api_option", "userdetails"));
+    Ok(serde_xml_rs::from_reader::<blocking::Response, info::Info>(v?)?.to_string())
+}
+
+fn handle_list(api_user_dev_key: String, api_user_key: String) -> Result<String, PastebinError> {
+    let v = api(multipart::Form::new()
+        .text("api_dev_key", api_user_dev_key)
+        .text("api_user_key", api_user_key)
+        .text("api_result_limit", "50")
+        .text("api_option", "list"));
+    Ok(
+        serde_xml_rs::from_reader::<blocking::Response, Vec<list::Paste>>(v?)?
+            .into_iter()
+            .map(|p| p.to_string())
+            .collect::<Vec<String>>()
+            .join("\n"),
+    )
+}
+
+fn handle_get(
+    sub_matches: &ArgMatches,
+    api_user_dev_key: String,
+    api_user_key: String,
+) -> Result<String, PastebinError> {
+    let paste_code = match sub_matches.get_one::<String>("CODE") {
+        Some(v) => v.to_string(),
+        None => "".to_string(),
+    };
+    let v = api(multipart::Form::new()
+        .text("api_dev_key", api_user_dev_key)
+        .text("api_user_key", api_user_key)
+        .text("api_paste_key", paste_code.clone())
+        .text("api_option", "show_paste"));
+    let text = v?.text()?;
+    if &text == "Bad API request, invalid permission to view this paste or invalid api_paste_key" {
+        Ok(get_public_paste(&paste_code)?.text()?)
+    } else {
+        Ok(text)
+    }
+}
+
+fn handle_delete(
+    sub_matches: &ArgMatches,
+    api_user_key: String,
+    api_user_dev_key: String,
+) -> Result<String, PastebinError> {
+    let code = match sub_matches.get_one::<String>("CODE") {
+        Some(v) => v.to_string(),
+        None => Err(PastebinError::Arg(
+            "code".to_string(),
+            "error getting the code".to_string(),
+        ))?,
+    };
+    let v = api(multipart::Form::new()
+        .text("api_dev_key", api_user_dev_key)
+        .text("api_user_key", api_user_key)
+        .text("api_option", "delete")
+        .text("api_paste_key", code.to_string()));
+    Ok(v?.text()?)
+}
+
+fn get_file(sub_matches: &ArgMatches) -> Result<String, PastebinError> {
+    let result = match sub_matches.get_one::<String>("FILE") {
+        Some(user_input) => {
+            let path = Path::new(user_input);
+            match path.to_str() {
+                None => Err(PastebinError::File(user_input.to_string()))?,
+                Some(p) => {
+                    if path.is_file() {
+                        fs::read_to_string(p)?.to_string()
+                    } else {
+                        Err(PastebinError::File(p.to_string()))?
+                    }
+                }
+            }
+        }
+        None => {
+            if atty::isnt(atty::Stream::Stdin) {
+                io::stdin()
+                    .lock()
+                    .lines()
+                    .map(|l| l.expect("Couldn't read from stdin"))
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            } else {
+                Err(PastebinError::File("".to_string()))?
+            }
+        }
+    };
+    Ok(result)
+}
+
+fn handle_new(
+    sub_matches: &ArgMatches,
+    api_user_dev_key: String,
+    api_user_key: String,
+) -> Result<String, PastebinError> {
+    let file = get_file(sub_matches)?;
+    let mut form = multipart::Form::new()
+        .text("api_dev_key", api_user_dev_key)
+        .text("api_option", "paste")
+        .text("api_paste_code", file);
+
+    if !sub_matches.get_flag("guest") {
+        form = form.text("api_user_key", api_user_key);
+    }
+
+    if let Some(expire) = sub_matches.get_one::<String>("expire") {
+        form = form.text("api_paste_expire_date", expire.to_string());
+    }
+
+    if let Some(title) = sub_matches.get_one::<String>("title") {
+        form = form.text("api_paste_name", title.to_string());
+    }
+
+    if let Some(level) = sub_matches.get_one::<String>("privacy") {
+        match Privacy::try_from(level.to_string()) {
+            Ok(l) => form = form.text("api_paste_private", l.form_ready()),
+            Err(e) => Err(PastebinError::Arg("privacy".to_string(), e))?,
+        };
+    }
+
+    if let Some(syntax) = sub_matches.get_one::<String>("syntax") {
+        form = form.text("api_paste_format", syntax.to_string());
+    }
+
+    let v = api(form);
+    Ok(v?.text()?)
+}
+
 fn match_command(
     matches: ArgMatches,
     api_user_dev_key: String,
     api_user_key: String,
 ) -> Result<(), PastebinError> {
-    match matches.subcommand() {
-        Some(("info", _)) => {
-            let v = api(multipart::Form::new()
-                .text("api_dev_key", api_user_dev_key)
-                .text("api_user_key", api_user_key)
-                .text("api_option", "userdetails"));
-            println!(
-                "{}",
-                serde_xml_rs::from_reader::<blocking::Response, info::Info>(v?)?
-            );
-            Ok(())
-        }
-        Some(("list", _)) => {
-            let v = api(multipart::Form::new()
-                .text("api_dev_key", api_user_dev_key)
-                .text("api_user_key", api_user_key)
-                .text("api_result_limit", "50")
-                .text("api_option", "list"));
-            serde_xml_rs::from_reader::<blocking::Response, Vec<list::Paste>>(v?)?
-                .into_iter()
-                .for_each(|p| println!("{}", p));
-            Ok(())
-        }
-        Some(("get", sub_matches)) => {
-            let paste_code = match sub_matches.get_one::<String>("CODE") {
-                Some(v) => v.to_string(),
-                None => "".to_string(),
-            };
-            let v = api(multipart::Form::new()
-                .text("api_dev_key", api_user_dev_key)
-                .text("api_user_key", api_user_key)
-                .text("api_paste_key", paste_code.clone())
-                .text("api_option", "show_paste"));
-            let text = v?.text()?;
-            if &text
-                == "Bad API request, invalid permission to view this paste or invalid api_paste_key"
-            {
-                println!("{}", get_public_paste(&paste_code)?.text()?);
-            } else {
-                println!("{}", text);
-            }
-            Ok(())
-        }
-        Some(("delete", sub_matches)) => {
-            let code = match sub_matches.get_one::<String>("CODE") {
-                Some(v) => v.to_string(),
-                None => Err(PastebinError::Arg("code".to_string(), "error getting the code".to_string()))?,
-            };
-            let v = api(multipart::Form::new()
-                .text("api_dev_key", api_user_dev_key)
-                .text("api_user_key", api_user_key)
-                .text("api_option", "delete")
-                .text("api_paste_key", code.to_string())
-            );
-            println!("{}", v?.text()?);
-            Ok(())
-        }
-        Some(("new", sub_matches)) => {
-            let file = match sub_matches.get_one::<String>("FILE") {
-                Some(user_input) => {
-                    let path = Path::new(user_input);
-                    match path.to_str() {
-                        None => Err(PastebinError::File(user_input.to_string()))?,
-                        Some(p) => {
-                            if path.is_file() {
-                                fs::read_to_string(p)?.to_string()
-                            } else {
-                                Err(PastebinError::File(p.to_string()))?
-                            }
-                        }
-                    }
-                }
-                None => {
-                    if atty::isnt(atty::Stream::Stdin) {
-                        io::stdin()
-                            .lock()
-                            .lines()
-                            .map(|l| l.expect("Couldn't read from stdin"))
-                            .collect::<Vec<String>>()
-                            .join("\n")
-                    } else {
-                        Err(PastebinError::File("".to_string()))?
-                    }
-                }
-            };
-            let mut form = multipart::Form::new()
-                .text("api_dev_key", api_user_dev_key)
-                .text("api_option", "paste")
-                .text("api_paste_code", file);
-
-            if !sub_matches.get_flag("guest") {
-                form = form.text("api_user_key", api_user_key);
-            }
-
-            if let Some(expire) = sub_matches.get_one::<String>("expire") {
-                form = form.text("api_paste_expire_date", expire.to_string());
-            }
-
-            if let Some(title) = sub_matches.get_one::<String>("title") {
-                form = form.text("api_paste_name", title.to_string());
-            }
-
-            if let Some(level) = sub_matches.get_one::<String>("privacy") {
-                match Privacy::try_from(level.to_string()) {
-                    Ok(l) => form = form.text("api_paste_private", l.form_ready()),
-                    Err(e) => Err(PastebinError::Arg("privacy".to_string(), e))?,
-                };
-            }
-
-            if let Some(syntax) = sub_matches.get_one::<String>("syntax") {
-                form = form.text("api_paste_format", syntax.to_string());
-            }
-
-            let v = api(form);
-            println!("{}", v?.text()?);
-
-            Ok(())
-        }
+    let result = match matches.subcommand() {
+        Some(("info", _)) => handle_info(api_user_dev_key, api_user_key),
+        Some(("list", _)) => handle_list(api_user_dev_key, api_user_key),
+        Some(("get", sub_matches)) => handle_get(sub_matches, api_user_dev_key, api_user_key),
+        Some(("delete", sub_matches)) => handle_delete(sub_matches, api_user_key, api_user_dev_key),
+        Some(("new", sub_matches)) => handle_new(sub_matches, api_user_dev_key, api_user_key),
         _ => unimplemented!(),
-    }
+    };
+    Ok(println!("{}", result?))
 }
 
 fn main() {
